181d20c79cfde7c407b797aa65d8ffd99b2475c9
huypn12 <huy.phung1292@gmail.com>
2019-08-15 12:09:01 +0200

    path finder

---
+++ src/path_finder.cpp
@@ -0,0 +1,13 @@
+#include "path_finder.hpp"
+
+#include <queue>
+
+using namespace org::kata;
+
+std::vector<int> path_finder::dijsktra(adj_list G, int src, int dst) {
+    std::queue<int> next;
+}
+
+std::unordered_map<int, int> path_finder::sssp(adj_list G, int src) {
+
+}
