8afea8feabdb599bc52b90053c21a3062a8b8a66
huypn12 <huy.phung@outlook.de>
2020-04-04 11:43:02 +0200

    Init

--- include/adt/binary_search_tree.h
+++
@@ -1,9 +0,0 @@
-
-namespace org::kata {
-class BinarySearchTree {
-public:
-  virtual int Search(int) = 0;
-  virtual void Delete(int) = 0;
-  virtual void Insert(int) = 0;
-};
-} // namespace org::kata
