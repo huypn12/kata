a3bae6121f18d669c45963190215e4a021ba04be
huypn12 <huy.phung@outlook.de>
2020-04-04 13:39:55 +0200

    Add GDMA practice

--- Competitive/simple-server/SimpleChatServer.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int on_mesg_recv() {
-    
-}
---
+++ Prep/chat-server/EventChatClient.cpp
@@ -0,0 +1,17 @@
+
+// std::thread and libevent
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+
+#include <event2/event.h>
+#include <event2/listener.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/keyvalq_struct.h>
+
+
+
+
