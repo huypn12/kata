1eab948c7ea496e4f071ec620369d828452460eb
huypn12 <huy.phung@outlook.de>
2020-04-28 00:38:15 +0200

    Daily practice

--- Competitive/OnlineProblems/stdafx.h
+++ Competitive/OnlineProblems/stdafx.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 /*
   My datastructure implementation
 */
+
+#define PrintArry(fmt, arr, n) \
+    { \
+        printf("[ "); \
+        for (int i = 0; i < n; i++) { \
+            printf("%##fmt", arr[i]); \
+        } \
+    }
